70101ac08a6a94d2449c461a92a05b606ab2d067
wfPython <wf_311h@163.com>
2018-03-07 15:17:22 +0800

    Initial commit

---
+++ firstdjango/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FirstdjangoConfig(AppConfig):
+    name = 'firstdjango'
---
+++ hello/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+#这里对应html文件
+def sousuo(request):
+    return render(request,'test.html')
+# Create your views here.
